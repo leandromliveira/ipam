a4d34bc60cdccfde4788716430d7856f3d80ab6e
leandro <leandro@pop-os.localdomain>
2022-01-26 15:07:20 -0300

    adiciona card dos distritos

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -8,7 +8,7 @@ export default function Dashboard() {
   const dispatch = useDispatch();
 
   const {
-    states, cities, setStates, setCities, setDistricts,
+    states, cities, districts, setStates, setCities, setDistricts,
   } = useContext(Context);
 
   const [stateId, setStateId] = useState('');
@@ -16,10 +16,12 @@ export default function Dashboard() {
 
   const estados = useSelector((state) => state.location.states);
   const cidades = useSelector((state) => state.location.cities);
+  const distritos = useSelector((state) => state.location.districts);
 
   useEffect(async () => {
     const response = await getStates();
     dispatch(actionStates(response));
+    setStateId(response[0].id);
   }, []);
 
   useEffect(() => {
@@ -30,39 +32,75 @@ export default function Dashboard() {
     setCities(cidades);
   }, [cidades]);
 
+  useEffect(() => {
+    setDistricts(distritos);
+  }, [distritos]);
+
   useEffect(async () => {
     const response = await getCity(stateId);
     dispatch(actionCities(response));
+    setCityId(response[0].id);
   }, [stateId]);
 
   useEffect(async () => {
     const response = await getDistrict(cityId);
     dispatch(actionDistricts(response));
-    setDistricts(useSelector((state) => state.location.districts));
   }, [cityId]);
 
-  // console.log(useSelector((state) => state.location.states));
-
   return (
-    <form>
-      <label htmlFor="state">
-        Estado:
-        {' '}
-        <select id="state" onChange={(event) => setStateId(event.target.value)}>
-          {states.map((state) => (
-            <option key={state.id} value={state.id}>{state.nome}</option>
-          ))}
-        </select>
-      </label>
-      <label htmlFor="city">
-        Município:
-        {' '}
-        <select id="city" onChange={(event) => setCityId(event.target.value)}>
-          {cities.map((city) => (
-            <option key={city.id} value={city.id}>{city.nome}</option>
-          ))}
-        </select>
-      </label>
-    </form>
+    <div>
+
+      <form>
+        <label htmlFor="state">
+          Estado:
+          {' '}
+          <select id="state" onChange={(event) => setStateId(event.target.value)}>
+            {states.map((state) => (
+              <option key={state.id} value={state.id}>{state.nome}</option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="city">
+          Município:
+          {' '}
+          <select id="city" onChange={(event) => setCityId(event.target.value)}>
+            {cities.map((city) => (
+              <option key={city.id} value={city.id}>{city.nome}</option>
+            ))}
+          </select>
+        </label>
+      </form>
+      <div>
+        {districts.map((district) => (
+          <div key={district.id}>
+            <p>
+              distrito:
+              {' '}
+              {district.nome}
+            </p>
+            <p>
+              microrregião:
+              {' '}
+              {district.municipio.microrregiao.nome}
+            </p>
+            <p>
+              mesorregião:
+              {' '}
+              {district.municipio.microrregiao.mesorregiao.nome}
+            </p>
+            <p>
+              UF:
+              {' '}
+              {district.municipio.microrregiao.mesorregiao.UF.sigla}
+            </p>
+            <p>
+              região:
+              {' '}
+              {district.municipio.microrregiao.mesorregiao.UF.regiao.nome}
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
