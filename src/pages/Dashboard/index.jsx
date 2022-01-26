ef5e94312007c1d6826e012b38dfccb9d1823224
leandro <leandro@pop-os.localdomain>
2022-01-26 13:06:08 -0300

    adiciona dropbox de municipio

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,26 +1,65 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { actionStates } from '../../redux/actions';
+import { actionCities, actionDistricts, actionStates } from '../../redux/actions';
 import Context from '../../services/Context';
-import { getStates } from '../../services/fetchAPI';
+import { getCity, getDistrict, getStates } from '../../services/fetchAPI';
 
 export default function Dashboard() {
   const dispatch = useDispatch();
-  const { states, setStates } = useContext(Context);
+
+  const {
+    states, cities, setStates, setCities, setDistricts,
+  } = useContext(Context);
+
+  const [stateId, setStateId] = useState('');
+  const [cityId, setCityId] = useState('');
+
+  const estados = useSelector((state) => state.location.states);
+  const cidades = useSelector((state) => state.location.cities);
 
   useEffect(async () => {
     const response = await getStates();
     dispatch(actionStates(response));
-    setStates(useSelector((state) => state.location.states));
   }, []);
+
+  useEffect(() => {
+    setStates(estados);
+  }, [estados]);
+
+  useEffect(() => {
+    setCities(cidades);
+  }, [cidades]);
+
+  useEffect(async () => {
+    const response = await getCity(stateId);
+    dispatch(actionCities(response));
+  }, [stateId]);
+
+  useEffect(async () => {
+    const response = await getDistrict(cityId);
+    dispatch(actionDistricts(response));
+    setDistricts(useSelector((state) => state.location.districts));
+  }, [cityId]);
+
+  // console.log(useSelector((state) => state.location.states));
+
   return (
     <form>
       <label htmlFor="state">
         Estado:
         {' '}
-        <select id="state">
+        <select id="state" onChange={(event) => setStateId(event.target.value)}>
           {states.map((state) => (
             <option key={state.id} value={state.id}>{state.nome}</option>
+          ))}
+        </select>
+      </label>
+      <label htmlFor="city">
+        Município:
+        {' '}
+        <select id="city" onChange={(event) => setCityId(event.target.value)}>
+          {cities.map((city) => (
+            <option key={city.id} value={city.id}>{city.nome}</option>
           ))}
         </select>
       </label>
